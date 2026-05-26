60e98add8910f9d12f95de9c39b0f6b8d6b4a91e
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-05-26 21:42:49 +0530

    built thread-safe shared state

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ mod cache;
 use std::time::Duration;
 use cache::{CacheItem,InmemoryCache};
 use std::thread::sleep;
+use std::sync::{Arc,Mutex};
 fn main() {
-    let mut cache=InmemoryCache::<String>::new();   //turbofish syntax
+    let mut cache=Arc::new(Mutex::new(InmemoryCache::<String>::new()));   //turbofish syntax
+   // let mut locked_cache=cache.lock().unwrap();   it will make the cache to be safe i.e lock till the end even when its sleep AND blocks the other threads completely.
+//    so directly use the .lock() method while calling the fn , in this way when the scope ends it gets unlock and make avail for other threads
+    
     println!("inserting item with 2 sec ttl");
-    cache.set(String::from("sathwik"), String::from("sathwik"), Duration::from_secs(2));
-    match cache.get("sathwik"){
+    cache.lock().unwrap().set(String::from("sathwik"), String::from("sathwik"), Duration::from_secs(2));
+    match cache.lock().unwrap().get("sathwik"){
         Some(val)=>{
             println!("{}",val);
         }
@@ -19,7 +23,7 @@ fn main() {
     sleep(Duration::from_secs(3));
 
     println!("fetching after sleep");
-    match cache.get("sathwik"){
+    match cache.lock().unwrap().get("sathwik"){
         Some(val)=>{
             println!("{}",val);
         }
